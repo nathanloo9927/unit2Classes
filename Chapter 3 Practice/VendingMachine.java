d0e5886c038fc1fb2f905853dd7b7221cd660059
nathanloo9927 <ngloo@NN123-HA-21.HS.sd203.org>
2015-09-15 15:09:19 -0500

    ruhroh

---
+++ Chapter 3 Practice/VendingMachine.java
@@ -0,0 +1,57 @@
+
+
+/**
+ * Write a description of class VendingMachine here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class VendingMachine
+{
+    /** description of instance variable x (add comment for each instance variable) */
+    private int cans;
+    private int tokens;
+
+    /**
+     * Default constructor for objects of class VendingMachine
+     */
+    public VendingMachine()
+    {
+        cans = 0;
+        tokens = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     * @param    y    description of parameter y
+     * @return    description of the return value
+     */
+    public void fillUp(int cans)
+    {
+        cans += 0;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public double findingCans()
+    {
+        return cans;
+    }
+
+
+}
