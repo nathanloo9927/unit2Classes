799e3cab151bf8515880018d99e1eba5470af6e2
nathanloo9927 <ngloo@NN123-HA-21.HS.sd203.org>
2015-09-17 15:01:22 -0500

    uhh

--- Chapter 3 Practice/VendingMachine.java
+++ Chapter 3 Practice/VendingMachine.java
@@ -50,7 +50,7 @@ public class VendingMachine
      */
     public int getCans()
     {
-        return cans;
+        return this.cans;
     }
 
     /**
@@ -64,9 +64,27 @@ public class VendingMachine
      * @param   y   description of parameter y
      * @return  description of the return value
      */
-    public void insertTokens()
+    public void insertTokens(int numberOfTokens)
     {
-        
+        tokens += numberOfTokens;
+        cans -= numberOfTokens;
     }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public int getTokens()
+    {
+        return this.tokens;
+    }
+
 
 }
